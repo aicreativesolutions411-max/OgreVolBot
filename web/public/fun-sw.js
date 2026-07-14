4002f5d70f5adaf2ff429cb0f16ea51b92e9d496
a.i.creativesolutions411-Max <a.i.creativesolutions411@gmail.com>
2026-07-13 19:22:45 -0700

    Refresh Fun profile assets

--- web/public/fun-sw.js
+++ web/public/fun-sw.js
@@ -1,10 +1,10 @@
 /* SlimeWire Fun: separate PWA shell and push worker. */
-const FUN_CACHE = "slimewire-fun-v7";
+const FUN_CACHE = "slimewire-fun-v8";
 const FUN_SHELL = [
   "/fun/",
   "/fun.html",
-  "/fun.css?v=11",
-  "/fun.js?v=20",
+  "/fun.css?v=12",
+  "/fun.js?v=21",
   "/fun-manifest.webmanifest?v=2",
   "/config.js",
   "/assets/slimewire/fun-app-icon-192.png",
